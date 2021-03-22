442c5e1612ff7516a6d761c4a39cee00688809cf
levshukova <olevshukova@gmail.com>
2021-03-22 18:24:35 +0200

    updated guard with some fixes

--- helpers/guard.js
+++ helpers/guard.js
@@ -4,7 +4,7 @@ const { HttpCode, Status } = require('./constants');
 
 const guard = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
-    const [, token] = req.get('Authorization').split(' ');
+    const token = req.get('Authorization')?.split(' ')[1];
 
     if (!user || err || token !== user.token) {
       return res.status(HttpCode.FORBIDDEN).json({
